cb7bf4997a5812469dba12c0ffdd42b1ce840943
MariettoFO <76008655+MariettoFO@users.noreply.github.com>
2021-02-04 20:48:52 +0100

    correct

--- src/app/signup/signup.page.ts
+++ src/app/signup/signup.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AlertController, LoadingController, NavController } from '@ionic/angular';
+import { AuthService } from '../services/auth.service';
+import firebase from 'firebase/app';
+import {AngularFireAuth} from '@angular/fire/auth'
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +12,93 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.page.scss'],
 })
 export class SignupPage implements OnInit {
+  validationUserMessage={
+    names:[
+      {type:"required", message:"Por favor, introduce tu nombre completo"}
+    ],
+    phone:[
+      {type:"required", message:"Por favor, introduce tu nº de teléfono"}
+    ],
+    email:[
+      {type:"required", message:"Por favor, introduce tu dirección de correo"},
+      {type:"pattern", message:"El Email introducido es incorecto. Prueba de nuevo"}
+    ],
+    password:[
+      {type:"required", message:"Por favor, introduce tu contraseña"},
+      {type:"minlength", message:"La contraseña debe tener al menos 6 caracteres"}
+    ]
+  }
 
-  constructor() { }
+  validationFormUser: FormGroup;
+  loading: any;
+
+  constructor(private nav: NavController,private router: Router, private navCtr: NavController, private formBuilder: FormBuilder, private authService: AuthService, public loadingCtrl: LoadingController, private alertCtrl: AlertController) { }
 
   ngOnInit() {
+    this.validationFormUser = this.formBuilder.group({
+      names: new FormControl('', Validators.compose([
+        Validators.required
+      ])),
+
+      phone: new FormControl('', Validators.compose([
+        Validators.required
+      ])),
+
+      email: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+      ])),
+
+      password: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(6)        
+      ]))
+
+    })
   }
+
+  registerUser(value){
+    this.showAlert();
+    try{
+      this.authService.userRegistration(value).then(response =>{
+        console.log(response);
+        if(response.user){
+          response.user.updateProfile({
+            displayName: value.names,
+            email: value.email,
+            phoneNumber: value.phone
+          });
+          this.loadingCtrl.dismiss();
+          this.router.navigate(['loginscreen']);
+        }
+      }, error=>{
+        this.loadingCtrl.dismiss();
+        this.errorLoading(error.message);
+      })
+    }catch(error){
+      console.log(error)
+    } 
+  }
+
+  async errorLoading(message: any){
+    const loading = await this.alertCtrl.create({
+      header: "Error al registrarse",
+      message: message,
+      buttons:[{
+        text:'ok',
+        handler:()=>{
+          this.navCtr.navigateBack(['signup'])
+        }
+      }]
+    })
+    await loading.present();
+  }
+
+  async showAlert(){
+    var load = await this.loadingCtrl.create({
+      message: "Por favor, espera..."
+    })
+  }
+  
 
 }
