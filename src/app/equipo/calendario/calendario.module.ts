2e568e138881b34e9132637e674cd99232cf2158
MFernandezOsorio <mario.fernandez.osorio.6@gmail.com>
2021-04-09 00:44:45 +0200

    act

--- src/app/equipo/calendario/calendario.module.ts
+++ src/app/equipo/calendario/calendario.module.ts
@@ -1,4 +1,4 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { CalendarioPageRoutingModule } from './calendario-routing.module';
 
 import { CalendarioPage } from './calendario.page';
+import { NgCalendarModule  } from 'ionic2-calendar';
 import { CalendarModule } from 'ion2-calendar';
 import {Component} from '@angular/core' 
 
@@ -16,7 +17,8 @@ import {Component} from '@angular/core'
     FormsModule,
     IonicModule,
     CalendarModule,
-    CalendarioPageRoutingModule,   
+    CalendarioPageRoutingModule, 
+    NgCalendarModule  
     
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
